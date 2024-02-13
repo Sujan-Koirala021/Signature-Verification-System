b02678c178fad09bb278cc670de88a32a8b55739
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-02-13 19:13:29 +0545

    Signature verification page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LandingPage from './pages/LandingPage'
+import Footer from './components/Footer/Footer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignatureVerification from './pages/SignatureVerification';
+import AboutPage from './pages/AboutPage';
+import HelpPage from './pages/HelpPage';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-red-500 w-full'>
-        Signature Verification System
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/verify" element={<SignatureVerification/>} />
+          <Route exact path="/about" element={<AboutPage/>} />
+          <Route exact path="/help" element={<HelpPage/>} />
+        </Routes>
+      </Router>
+      <Footer />
     </>
   )
 }
