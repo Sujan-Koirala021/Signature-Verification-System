7bc87eda5303c6ec2777d24b750c7d4cfe00afcf
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-02-19 16:04:45 +0545

    yolov8 working from backend

--- server/controller/verifyUsingDocumentController.js
+++ server/controller/verifyUsingDocumentController.js
@@ -1,5 +1,25 @@
-exports.verifyUsingDocument = async (req ,res ,next) => {
-    console.log(req.body);
-    console.log("Extract Signature")
-    res.send("Hello World");
+const { PythonShell } = require('python-shell');
+
+exports.verifyUsingDocument = async (req, res, next) => {
+    let options = {
+        scriptPath: "C:\\Users\\Dell\\Desktop\\Minor-Project\\Signature-extraction\\yolov8",
+    };
+    // python detect.py --source image7.jpg --weights 'best.pt' --save-txt --save-crop --hide-labels --hide-conf --classes 1 --line-thickness 2
+    await PythonShell.run("predict.py", options, (err, res) => {
+        if (err) console.log(err)
+        if (res) console.log(res)
+    });
+
+    // try {
+    //     const results = await PythonShell.run("predict.py", options);
+    //     console.log("Python script executed successfully.");
+    //     console.log("Python script output:", results[0]);
+
+    //     console.log(req.body);
+    //     console.log("Extract Signature");
+    //     res.send("Hello World");
+    // } catch (err) {
+    //     console.error("Error occurred while running predict.py:", err);
+    //     return res.status(500).send("Error occurred while processing the document.");
+    // }
 };
