5fb3954c4dacaad838e745d41dbb3d6b5e145fa6
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-02-20 18:02:14 +0545

    signature extraction with provided image

--- server/controller/verifyUsingDocumentController.js
+++ server/controller/verifyUsingDocumentController.js
@@ -1,15 +1,22 @@
+const os = require('os');
+// const multer = require('multer');
+// const upload = multer({ dest: 'uploads/verification' });
+const path = require('path');
 const { PythonShell } = require('python-shell');
+
+const currentDirectory = process.cwd();
 
 exports.verifyUsingDocument = async (req, res, next) => {
     let options = {
-        scriptPath: "C:\\Users\\Dell\\Desktop\\Minor-Project\\Signature-extraction\\yolov8",
+        scriptPath: path.join(currentDirectory, '..', 'Signature-extraction', 'yolov8')
     };
     // python detect.py --source image7.jpg --weights 'best.pt' --save-txt --save-crop --hide-labels --hide-conf --classes 1 --line-thickness 2
     await PythonShell.run("predict.py", options, (err, res) => {
         if (err) console.log(err)
         if (res) console.log(res)
     });
-
+    
+    await res.send("Finished Processing");
     // try {
     //     const results = await PythonShell.run("predict.py", options);
     //     console.log("Python script executed successfully.");
